fc33abf00d12e26692491d44332a0999cd0350d4
Alessandro Piredda <gd.apiredda@gmail.com>
2019-09-23 19:12:42 +0200

    UPDATE - Assignment Alessandro Piredda

--- sketch.js
+++ sketch.js
@@ -4,34 +4,114 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  background(220);
+  background(5);
   angleMode(DEGREES);
-  frameRate(40);
+  frameRate(60);
   // put setup code here
 }
 
 function draw() {
-  translate((windowWidth/2)-250,(windowHeight/2)-250);
+/////////////////////
+  push();
 
+  translate((windowWidth/2)-250,(windowHeight/2)-250);
   strokeWeight(0.5);
-  stroke(lerpColor(color("red"),color("blue"),frameCount/125));
+  stroke(lerpColor(color("red"),color("blue"),frameCount/150));
   points = 200;
   pointAngle = (360/points);
   raggio = 250;
   angle=0;
   angle= angle+pointAngle;
 
-  x =(cos(frameCount*angle) * raggio);
-  y =(sin(frameCount*angle) * raggio);
-	line(raggio+250, 250,x+250,y+250);
+  x =(cos((frameCount)*angle) * raggio);
+  y =(sin((frameCount)*angle) * raggio);
+	line(raggio+250,250,x+250,y+250);
+
+  pop();
+/////////////////////
+  push();
+
+  translate((windowWidth/2)-250,(windowHeight/2)-250);
+
+  strokeWeight(0.5);
+  stroke(lerpColor(color("blue"),color("red"),frameCount/150));
+  points1 = 200;
+  pointAngle1 = (360/points1);
+  raggio1 = -250;
+  angle1=0;
+  angle1= angle1+pointAngle1;
+
+  x =(cos(frameCount*angle1) * raggio1);
+  y =(sin(frameCount*angle1) * raggio1);
+	line(0,250,x+250,y+250);
+
+  pop();
+///////////////////////
+  push();
+
+  translate((windowWidth/2)+300,(windowHeight/2)-300);
+
+  strokeWeight(0.5);
+  stroke(lerpColor(color("yellow"),color("violet"),frameCount/100));
+  points2 = 200;
+  pointAngle2 = (360/points2);
+  raggio2 = 100;
+  angle2=0;
+  angle2= angle2+pointAngle2;
+
+  x =(cos(frameCount*angle2) * raggio2);
+  y =(sin(frameCount*angle2) * raggio2);
+  line(raggio2+100,100,x+100,y+100);
+
+  pop();
+//////////////////////
+  push();
+
+  translate((windowWidth/2)+300,(windowHeight/2)-300);
+
+  strokeWeight(0.5);
+  stroke(lerpColor(color("violet"),color("yellow"),frameCount/100));
+  points2 = 200;
+  pointAngle2 = (360/points2);
+  raggio2 = -100;
+  angle2=0;
+  angle2= angle2+pointAngle2;
+
+  x =(cos(frameCount*angle2) * raggio2);
+  y =(sin(frameCount*angle2) * raggio2);
+  line(0,100,x+100,y+100);
+
+  pop();
+//////////////////////
+
+ellipse(mouseX,mouseY,1,1);
+noStroke();
+
+//////////////////
+
+textAlign(LEFT);
+drawWords(width * 0.05);
+
+textAlign(RIGHT);
+drawWords2(width * 0.95);
 
 
-  if(frameCount == 200){
-      noLoop();
-    }
+
 }
 
+function drawWords(x){
+  fill(255);
+  textSize(20);
+  text('MOVE YOUR MOUSE TO CREATE AWESOME STARS!', x, 100);
+}
+
+function drawWords2(x){
+  fill(255);
+  textSize(20);
+  text('PRESS IT AND YOU DESTROY THEM LIKE A POWERFUL GOD!', x, 670);
+}
+
+
 function mousePressed(){
-  if (frameCount == 200)
-  remove();
+  background(5);
 }
